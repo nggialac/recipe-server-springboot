055614bfafffc6a960a442cc3e5c8275f0450a30
Nguyen Gia Lac <nggialac99@gmail.com>
2021-04-23 20:47:57 +0700

    fixxx

--- src/main/java/com/lacnguyen/recipeserver/config/SecurityConfig.java
+++ src/main/java/com/lacnguyen/recipeserver/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().and().csrf().disable();
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
     }
 
 //    @Bean
